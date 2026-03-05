8d42dc7d4d8c36dd08b2b0c86abca965901b1443
hlajak06 <hlajak06@gasos-ro.cz>
2026-03-05 10:41:00 +0100

    zubma

--- src/main/java/com/example/ukoly/HlavniController.java
+++ src/main/java/com/example/ukoly/HlavniController.java
@@ -4,13 +4,10 @@ import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class HlavniController {
-    ObservableList<String> observableList = FXCollections.observableArrayList();
+    ObservableList<Ukol> observableList = FXCollections.observableArrayList();
 
     @FXML
     private TextField nazevTextField;
@@ -22,19 +19,77 @@ public class HlavniController {
     private Label detailLabel;
 
     @FXML
-    private RadioButton ajRadio;
+    private ToggleGroup subject;
 
     @FXML
-    private RadioButton cjRadio;
+    private ToggleGroup priority;
 
     @FXML
-    private RadioButton ckRadio;
+    private CheckBox finishedCheckBox;
 
+
+
+    @FXML
+    public void initialize() {
+        ukolyListView.setItems(observableList);
+    }
 
     @FXML
     protected void handlePridatUkol() {
-        observableList.add(nazevTextField.getText());
+        RadioButton selectedSubject = (RadioButton) subject.getSelectedToggle();
 
+        RadioButton selectedPriority = (RadioButton) priority.getSelectedToggle();
+
+        observableList.add(new Ukol(nazevTextField.getText(), selectedSubject.getText(), selectedPriority.getText(), finishedCheckBox.isSelected() ));
+
+
+    }
+
+    @FXML
+    protected void handleShowDetail() {
+        if (ukolyListView.getSelectionModel().getSelectedItem() != null) {
+            Ukol selectedUkol = (Ukol) ukolyListView.getSelectionModel().getSelectedItem();
+
+            detailLabel.setText("Název: " + selectedUkol.getName() + " , Předmět: " + selectedUkol.getSubject() + " , Priorita: " + selectedUkol.getPriority() + " , Dokončeno: " + selectedUkol.isFinished());
+        }
+        else {
+            detailLabel.setText("Nebyl vybrán úkol");
+        }
+    }
+
+    @FXML
+    protected void handleEnd() {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void handleShowAll() {
         ukolyListView.setItems(observableList);
+    }
+
+    @FXML
+    protected void handleShowCompleted() {
+        ObservableList<Ukol> tempList = FXCollections.observableArrayList();
+
+        for (Ukol ukol : observableList) {
+            if  (ukol.isFinished()) {
+                tempList.add(ukol);
+            }
+        }
+
+        ukolyListView.setItems(tempList);
+    }
+
+    @FXML
+    protected void handleShowUnfinished() {
+        ObservableList<Ukol> tempList = FXCollections.observableArrayList();
+
+        for (Ukol ukol : observableList) {
+            if  (!ukol.isFinished()) {
+                tempList.add(ukol);
+            }
+        }
+
+        ukolyListView.setItems(tempList);
     }
 }
--- src/main/java/com/example/ukoly/Ukol.java
+++ src/main/java/com/example/ukoly/Ukol.java
@@ -1,4 +1,40 @@
 package com.example.ukoly;
 
 public class Ukol {
+    private String name;
+    private String subject;
+    private String priority;
+    private boolean finished;
+
+    public Ukol(String name, String subject, String priority,  boolean finished) {
+        this.name = name;
+        this.subject = subject;
+        this.priority = priority;
+        this.finished = finished;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    // "Název: " + name + " , Předmět: " + subject + " , Priorita: " + priority + " , Dokončeno: " + finished;
+
+
 }
