d4abd8b1b0a08baeabdbef9a3f2e1cb652e77f19
hlajak06 <hlajak06@gasos-ro.cz>
2026-03-06 09:34:55 +0100

    e

--- src/main/java/com/example/ukoly/HlavniController.java
+++ src/main/java/com/example/ukoly/HlavniController.java
@@ -1,10 +1,16 @@
 package com.example.ukoly;
 
-import javafx.beans.Observable;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HlavniController {
     ObservableList<Ukol> observableList = FXCollections.observableArrayList();
@@ -27,20 +33,61 @@ public class HlavniController {
     @FXML
     private CheckBox finishedCheckBox;
 
+    @FXML
+    private Button upravitButton;
 
+    @FXML
+    private Button smazatButton;
+
+    @FXML
+    private  Button infoButton;
+
+
+
+    public void showInfo() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Tutorial Dialog");
+        alert.setHeaderText("Nápověda k použití aplikace"); // Set to null for no header
+        alert.setContentText("Nejdříve zadáme název ůkolu a poté jaký je to předmět a jakou má úkol prioritu a jestli je splněn. \n" +
+                " Poté můžeme vybrat položku ze seznamu po čemž můžeme zobrazit podrobné info, nebo ho smazat, a nebo ho upravit (čímž ho přepíšeme)");
+
+        alert.showAndWait();
+    }
 
     @FXML
     public void initialize() {
         ukolyListView.setItems(observableList);
+        upravitButton.setDisable(true);
+        smazatButton.setDisable(true);
+        infoButton.setDisable(true);
     }
+
 
     @FXML
     protected void handlePridatUkol() {
-        RadioButton selectedSubject = (RadioButton) subject.getSelectedToggle();
+        if (nazevTextField.getText().length() > 0 && subject.getSelectedToggle() != null && priority.getSelectedToggle() != null) {
+            boolean temp = true;
+            for (Ukol ukoly : observableList) {
+                if (ukoly.getName().equals(nazevTextField.getText())){
+                    temp = false;
+                }
+            }
+            if (temp) {
+                RadioButton selectedSubject = (RadioButton) subject.getSelectedToggle();
 
-        RadioButton selectedPriority = (RadioButton) priority.getSelectedToggle();
+                RadioButton selectedPriority = (RadioButton) priority.getSelectedToggle();
 
-        observableList.add(new Ukol(nazevTextField.getText(), selectedSubject.getText(), selectedPriority.getText(), finishedCheckBox.isSelected() ));
+                observableList.add(new Ukol(nazevTextField.getText(), selectedSubject, selectedPriority, finishedCheckBox.isSelected() ));
+
+                ukolyListView.setItems(observableList);
+                ukolyListView.refresh();
+            }
+
+
+        }
+        else {
+            detailLabel.setText("Formulář není zcela vyplněný!");
+        }
 
 
     }
@@ -50,7 +97,7 @@ public class HlavniController {
         if (ukolyListView.getSelectionModel().getSelectedItem() != null) {
             Ukol selectedUkol = (Ukol) ukolyListView.getSelectionModel().getSelectedItem();
 
-            detailLabel.setText("Název: " + selectedUkol.getName() + " , Předmět: " + selectedUkol.getSubject() + " , Priorita: " + selectedUkol.getPriority() + " , Dokončeno: " + selectedUkol.isFinished());
+            detailLabel.setText("Název: " + selectedUkol.getName() + " , Předmět: " + selectedUkol.getSubjectString() + " , Priorita: " + selectedUkol.getPriorityString() + " , Dokončeno: " + selectedUkol.isFinished());
         }
         else {
             detailLabel.setText("Nebyl vybrán úkol");
@@ -59,7 +106,7 @@ public class HlavniController {
 
     @FXML
     protected void handleEnd() {
-        System.exit(0);
+        Platform.exit();
     }
 
     @FXML
@@ -91,5 +138,194 @@ public class HlavniController {
         }
 
         ukolyListView.setItems(tempList);
+    }
+
+    @FXML
+    protected void handleSelected() {
+        if (ukolyListView.getSelectionModel().getSelectedItem() != null) {
+            Ukol selectedUkol = (Ukol) ukolyListView.getSelectionModel().getSelectedItem();
+
+            nazevTextField.setText(selectedUkol.getName());
+
+            subject.selectToggle(selectedUkol.getSubject());
+
+            priority.selectToggle(selectedUkol.getPriority());
+
+            finishedCheckBox.setSelected(selectedUkol.isFinished());
+
+            handleShowDetail();
+
+            upravitButton.setDisable(false);
+            smazatButton.setDisable(false);
+            infoButton.setDisable(false);
+        }
+        else {
+            detailLabel.setText("Nebyl vybrán úkol");
+        }
+    }
+
+    @FXML
+    protected void handleChange() {
+        if (ukolyListView.getSelectionModel().getSelectedItem() != null) {
+            Ukol selectedUkol = (Ukol) ukolyListView.getSelectionModel().getSelectedItem();
+
+            RadioButton selectedSubject = (RadioButton) subject.getSelectedToggle();
+
+            RadioButton selectedPriority = (RadioButton) priority.getSelectedToggle();
+
+            selectedUkol.setName(nazevTextField.getText());
+            selectedUkol.setSubject(selectedSubject);
+            selectedUkol.setPriority(selectedPriority);
+            selectedUkol.setFinished(finishedCheckBox.isSelected());
+
+
+
+
+            detailLabel.setText("Detail");
+        }
+        else {
+            detailLabel.setText("Nebyl vybrán úkol");
+        }
+    }
+
+    @FXML
+    protected void handleDelete() {
+        if (ukolyListView.getSelectionModel().getSelectedItem() != null) {
+
+            observableList.remove(ukolyListView.getSelectionModel().getSelectedItem());
+
+            detailLabel.setText("Detail");
+
+            if (observableList.isEmpty()) {
+                upravitButton.setDisable(true);
+                smazatButton.setDisable(true);
+                infoButton.setDisable(true);
+            }
+
+        }
+        else {
+            detailLabel.setText("Nebyl vybrán úkol");
+        }
+    }
+
+    @FXML
+    protected void handleSave() {
+        if (!observableList.isEmpty()) {
+            ArrayList<String> temp = new ArrayList<>();
+            Path soubor = Path.of("data", "log.txt");
+
+            try {
+
+                // Pokud složka neexistuje, vytvoříme ji
+                if (!Files.exists(soubor.getParent())) {
+                    Files.createDirectories(soubor.getParent());
+                }
+
+
+                for  (Ukol ukol : observableList) {
+                    temp.add(ukol.toString2());
+                }
+
+
+                // Zápis do souboru (vytvoří nebo přepíše soubor)
+                Files.write(soubor, temp);
+
+                System.out.println("Zápis proběhl úspěšně.");
+                System.out.println("Absolutní cesta: " + soubor.toAbsolutePath());
+
+            } catch (IOException e) {
+                System.out.println("Chyba při zápisu do souboru:");
+                e.printStackTrace();
+            }
+        }
+        else {
+            detailLabel.setText("Není co uložit");
+            System.err.println("Není co uložit");
+        }
+    }
+
+    @FXML
+    protected void handleLoad() {
+        try {
+            ArrayList<String> temp = new ArrayList<>();
+            Path soubor = Path.of("data", "log.txt");
+
+
+            String name;
+            ToggleButton selectedSubject = null;
+            String selectedSubjectString;
+            ToggleButton selectedPriority = null;
+            String selectedPriorityString;
+            boolean finishedCheckBox;
+
+            if (soubor.toFile().exists()) {
+                observableList.removeAll(observableList);
+
+                try {
+                    Files.readAllLines(soubor).forEach(temp::add);
+                } catch (IOException e) {
+                    System.out.println("Chyba při zápisu do souboru:");
+                    e.printStackTrace();
+                }
+
+                for  (String s : temp) {
+                    System.out.println(s);
+                    StringBuilder stringBuilder = new StringBuilder(s);
+
+                    name =  stringBuilder.substring(0,stringBuilder.indexOf(";"));
+
+                    System.out.println("Name: " + name);
+
+                    stringBuilder.delete(0, stringBuilder.indexOf(";")+1);
+
+                    selectedSubjectString = stringBuilder.substring(0,stringBuilder.indexOf(";"));
+
+                    System.out.println("Subject: " + selectedSubjectString);
+
+                    stringBuilder.delete(0, stringBuilder.indexOf(";")+1);
+
+                    selectedPriorityString = stringBuilder.substring(0,stringBuilder.indexOf(";"));
+
+                    System.out.println("Priority: " + selectedPriorityString);
+
+                    stringBuilder.delete(0, stringBuilder.indexOf(";")+1);
+
+                    if (stringBuilder.toString().equals("true")) {
+                        finishedCheckBox = true;
+                    }
+                    else {
+                        finishedCheckBox = false;
+                    }
+
+                    System.out.println("Finished: " + finishedCheckBox);
+
+                    stringBuilder.delete(0, stringBuilder.length());
+
+
+                    for (Toggle toggle : subject.getToggles()) {
+                        RadioButton rb = (RadioButton) toggle;
+                        if (rb.getText().equals(selectedSubjectString)) {
+                            selectedSubject = rb;
+                            System.out.println("Selected set: " + rb.getText());
+                            break; // Exit once we find the match
+                        }
+                    }
+
+                    for (Toggle toggle : priority.getToggles()) {
+                        RadioButton rb = (RadioButton) toggle;
+                        if (rb.getText().equals(selectedPriorityString)) {
+                            selectedPriority = rb;
+                            System.out.println("Priority set: " + rb.getText());
+                            break; // Exit once we find the match
+                        }
+                    }
+
+                    observableList.add(new Ukol(name, selectedSubject, selectedPriority, finishedCheckBox));
+                }
+            }
+        }
+        catch (Exception e) {
+            System.err.println("Něco je špatně s formátem dat v souboru. |" + e.getMessage());
+        }
     }
 }
--- src/main/java/com/example/ukoly/Ukol.java
+++ src/main/java/com/example/ukoly/Ukol.java
@@ -1,16 +1,24 @@
 package com.example.ukoly;
 
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
 public class Ukol {
     private String name;
-    private String subject;
-    private String priority;
+    private ToggleButton subject;
+    private String subjectString;
+    private ToggleButton priority;
+    private String priorityString;
     private boolean finished;
 
-    public Ukol(String name, String subject, String priority,  boolean finished) {
+    public Ukol(String name, ToggleButton subject, ToggleButton priority,  boolean finished) {
         this.name = name;
         this.subject = subject;
         this.priority = priority;
         this.finished = finished;
+
+        subjectString = subject.getText();
+        priorityString = priority.getText();
     }
 
 
@@ -18,20 +26,45 @@ public class Ukol {
         return name;
     }
 
-    public String getSubject() {
-        return subject;
+    public String getSubjectString() {
+        return subjectString;
     }
 
-    public String getPriority() {
-        return priority;
+    public ToggleButton getSubject() {return subject;}
+
+    public String getPriorityString() {
+        return priorityString;
     }
+
+    public ToggleButton getPriority() {return priority;}
 
     public boolean isFinished() {
         return finished;
     }
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(ToggleButton subject) {
+        this.subject = subject;
+    }
+
+    public void setPriority(ToggleButton priority) {
+        this.priority = priority;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
     public String toString() {
         return name;
+    }
+
+    public String toString2() {
+        return name + ";" +  subjectString.toString() + ";" + priorityString.toString() + ";" + finished;
     }
 
     // "Název: " + name + " , Předmět: " + subject + " , Priorita: " + priority + " , Dokončeno: " + finished;
